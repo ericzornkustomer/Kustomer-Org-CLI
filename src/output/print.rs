3ed4d89c8a1df6fcdc18fe0b08987a0588a14179
Eric Zorn <eric.zorn@kustomer.com>
2022-12-09 16:51:49 -0500

    fix: print

--- src/output/print.rs
+++ src/output/print.rs
@@ -5,7 +5,9 @@ use crate::models::org_domain_response::OrgDomainResponse;
 pub fn print_org_domain_response(domain_response: &OrgDomainResponse) {
     let id = domain_response.data.id.clone();
     let org_name = domain_response.data.attributes.name.clone();
+    let org_name = org_name.to_uppercase();
+    let pod_name = domain_response.data.attributes.pod.to_uppercase();
 
-    let success_message = format!("{} has org id - {}", org_name.to_uppercase(), id).green();
+    let success_message = format!("{} has Org ID: {} in Pod: {}", org_name, id, pod_name).green();
     println!("{success_message}");
 }
