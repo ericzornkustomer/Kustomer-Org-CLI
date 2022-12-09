9c557b077c50dcd92c038f201e393ec8d3096435
Eric Zorn <eric.zorn@kustomer.com>
2022-12-09 16:13:00 -0500

    chore: adding further support

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 
 use kustomer_org::get_org_domain_data;
-use kustomer_org::get_full_org_data;
+// use kustomer_org::get_full_org_data;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -18,15 +18,18 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     match opt {
         KustomerOrgData { org_name } => {
-            let domain = get_org_domain_data(&org_name).await;
+            let org_name_lower = org_name.to_lowercase();
+            let domain = get_org_domain_data(&org_name_lower).await;
 
             match domain {
                 Ok(org_domain_data) => {
-                    let org_id = org_domain_data.data.id.clone();
-                    println!("{:#?} has org id - {org_id}", org_domain_data);
+                    let id = org_domain_data.data.id;
+                    let attrs = &org_domain_data.data.attributes;
+
+                    println!("{} has org id - {}", attrs.name, id);
                     
                     // TODO: Implement
-                    get_full_org_data(&org_domain_data).await?;
+                    // get_full_org_data(&org_domain_data).await?;
                 }
                 Err(_e) => println!("having trouble fetching Kustomer org data for org {}", org_name)
             }
