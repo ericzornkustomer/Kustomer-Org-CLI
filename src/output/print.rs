11316b202a4a799d9958fc12d1ddeed45198d693
Eric Zorn <eric.zorn@kustomer.com>
2022-12-14 11:27:24 -0500

    fix: org id variable reference

--- src/output/print.rs
+++ src/output/print.rs
@@ -3,11 +3,11 @@ use colored::Colorize;
 use crate::models::org_domain_response::OrgDomainResponse;
 
 pub fn print_org_domain_response(domain_response: &OrgDomainResponse) {
-    let id = domain_response.data.id.clone();
+    let org_id = domain_response.data.attributes.org_id.clone();
     let org_name = domain_response.data.attributes.name.clone();
     let org_name = org_name.to_uppercase();
     let pod_name = domain_response.data.attributes.pod.to_uppercase();
 
-    let success_message = format!("{} has Org ID: {} in Pod: {}", org_name, id, pod_name).green();
+    let success_message = format!("{} has Org ID: {} in Pod: {}", org_name, org_id, pod_name).green();
     println!("{success_message}");
 }
